d88fb2b34902872e68490dd5c6ea8a2681e52f4c
RootDroid <42697021+R00tDroid@users.noreply.github.com>
2024-04-11 20:00:41 +0200

    feature/config (#1)
    Co-authored-by: R00tdroid <info@rootdroid.nl>

--- src/main/java/nl/rootdroid/tweakablelevels/mixin/ExperienceMixin.java
+++ src/main/java/nl/rootdroid/tweakablelevels/mixin/ExperienceMixin.java
@@ -1,6 +1,8 @@
 package nl.rootdroid.tweakablelevels.mixin;
 
 import net.minecraft.entity.player.PlayerEntity;
+import nl.rootdroid.tweakablelevels.ModConfig;
+import nl.rootdroid.tweakablelevels.TweakableLevels;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,8 +10,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public class ExperienceMixin {
-	@Inject(at = @At("HEAD"), method = "getNextLevelExperience", cancellable = true)
-	private void getNextLevelExperience(CallbackInfoReturnable<Integer> returnable) {
-		returnable.setReturnValue(7);
-	}
+    @Inject(at = @At("HEAD"), method = "getNextLevelExperience", cancellable = true)
+    private void getNextLevelExperience(CallbackInfoReturnable<Integer> returnable) {
+        PlayerEntity playerEntity = (PlayerEntity)(Object)this;
+        ModConfig config = TweakableLevels.getConfig();
+        int experience = config.getRequiredExperience(playerEntity.experienceLevel);
+        returnable.setReturnValue(experience);
+    }
 }
